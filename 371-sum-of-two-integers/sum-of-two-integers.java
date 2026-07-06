a115e36a40a1bc6ffb0976ab115e3cf1c85ad4e9
Subasree812 <141218729+Subasree812@users.noreply.github.com>
2026-07-06 15:33:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (32.37%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -3,10 +3,37 @@ class Solution {
         if(a == 0) return b;
         if(b == 0) return a;
         while ( b != 0){
-            int c = a & b;
-            a = a ^ b;
-            b = c << 1;
+            int c = a & b; // it will give the value of carry
+            a = a ^ b; // it will store the xor value of 2 numbers 
+            b = c << 1; // carry will be shifted
         }
+
+        /*5+3=8
+        0101 (5)
+        0011 (3)
+        ----------
+        0001 (1) carry  &
+        ----------
+        the carry is shifted left it become 2 
+
+        0101 (5)
+        0011 (3)
+        --------
+        0110 (6)  sum ^
+        ---------
+         
+        till b become 0 it will continue
+        getSum(5,3)
+        ↓
+        getSum(6,2)
+        ↓
+        getSum(4,4)
+        ↓
+        getSum(0,8)
+        ↓
+        getSum(8,0)
+        */
+
         return a;
     }
 }
